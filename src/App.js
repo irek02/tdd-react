3cffbad38c849b812479a35f72c183d6770ad0f1
Irek Mirgaleev <irek.mirgaleev@acquia.com>
2020-08-18 07:32:15 -0400

    wip

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Card, Button } from 'react-bootstrap';
 
 function App() {
+
+  const homesData = [
+    {
+      "title": "Home 1",
+      "image": "assets/listing.jpg",
+      "location": "new york",
+      "price": "125"
+    },
+    {
+      "title": "Home 2",
+      "image": "assets/listing.jpg",
+      "location": "boston",
+      "price": "225"
+    },
+    {
+      "title": "Home 3",
+      "image": "assets/listing.jpg",
+      "location": "chicago",
+      "price": "325"
+    }
+  ];
+
+  const homes = homesData.map((home, i) => <Home name={home} key={i}/>);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      { homes }
     </div>
+  );
+}
+
+function Home(props) {
+  return (
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the bulk of
+          the card's content.
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
   );
 }
 
