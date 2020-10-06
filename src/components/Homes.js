f4088247152739a496d5680611e64cd7802c5da8
Irek Mirgaleev <irek.mirgaleev@acquia.com>
2020-10-06 07:28:42 -0400

    wip

--- src/components/Homes.js
+++ src/components/Homes.js
@@ -1,22 +1,58 @@
 import React, { useEffect, useState } from 'react';
 
+// const homesData = [
+//   {
+//     "title": "Home 1",
+//     "image": "listing.jpg",
+//     "location": "new york",
+//     "price": "125"
+//   },
+//   {
+//     "title": "Home 2",
+//     "image": "listing.jpg",
+//     "location": "boston",
+//     "price": "225"
+//   },
+//   {
+//     "title": "Home 3",
+//     "image": "listing.jpg",
+//     "location": "chicago",
+//     "price": "325"
+//   }
+// ];
+
+let homesData = [];
+
 export default function Homes() {
 
   // const [homesState, setHomesState] = useState([]);
 
-  // useEffect(() => {
 
-  //   // fetch('foo/bar')
-  //   //   .then(homes => console.log(homes));
+  useEffect(() => {
 
-  // });
+    fetch('https://run.mocky.io/v3/6474432c-4bae-4807-bfbe-427b252f0b76')
+      .then(homes => homes.json())
+      .then(homes => homesData = homes);
 
+  });
+
+  const homes = homesData.map((home, i) => {
+    return (
+      <div data-testid="home" key={ i }>
+        <img data-testid="image" src={ home.image } alt="" />
+        <div data-testid="title">
+          { home.title }
+        </div>
+        <div data-testid="location">
+          { home.location }
+        </div>
+      </div>
+    );
+  });
 
   return (
     <>
-      <div data-testid="home"></div>
-      <div data-testid="home"></div>
-      <div data-testid="home"></div>
+      { homes }
     </>
   );
 }
