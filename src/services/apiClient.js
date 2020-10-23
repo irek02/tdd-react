5630bd6bac9a20febc17d741be817632edf5048d
Irek Mirgaleev <irek.mirgaleev@acquia.com>
2020-10-23 07:58:32 -0400

    wip

---
+++ src/services/apiClient.js
@@ -0,0 +1,12 @@
+const apiClient = {
+
+  getHomes: () => {
+    // Make the real API call to get homes.
+    return fetch('https://run.mocky.io/v3/62de12a6-dce1-4b9c-a34c-c77e275df98a')
+      .then(response => response.json());
+
+  }
+
+}
+
+export default apiClient;
