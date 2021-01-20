bb4c9334275753587681ad907390163da91c7c05
Irek Mirgaleev <irek.mirgaleev@acquia.com>
2021-01-20 07:23:44 -0500

    Revert "Merge branch '03_01_first_test' into 03_02_homes_data"
    This reverts commit f7bee2bdc78f0b8bebd9715662215693cfb18fe2, reversing
    changes made to 066a28f92390ece74eaa53eebf396b291876d964.

--- src/components/Homes.js
+++ src/components/Homes.js
@@ -3,40 +3,11 @@ import React from 'react';
 export default function Homes() {
 
   // const homesDataPromise = apiClient.getHomes();
-  const homesDataPromise = Promise.resolve([
-    {
-      title: "Test home 1",
-      image: "listing.jpg",
-      location: "Test location 1",
-      price: "1",
-    },
-    {
-      title: "Test home 2",
-      image: "listing.jpg",
-      location: "Test location 2",
-      price: "2",
-    },
-    {
-      title: "Test home 3",
-      image: "listing.jpg",
-      location: "Test location 3",
-      price: "3",
-    }
-  ]);
-
-  let homes;
-
-  homesDataPromise.then(homesData => {
-    homes = homesData.map(home => {
-      return (
-        <div data-testid="home">Home!</div>
-      );
-    });
-  });
+  const homesDataPromise = Promise.resolve([]);
 
   return (
     <div>
-      { homes }
+      <div data-testid="home">Home!</div>
     </div>
   );
 }
